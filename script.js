ec10b0a2230da7942877f7456efe25daf21baca7
Reyezando <reynardo.etantyo2@gmail.com>
2025-11-12 13:58:58 +0700

    update body

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
+// Open Full Image
+document.querySelectorAll('.body__content__house').forEach(content => {
+  content.addEventListener("click", function () {
+    document.querySelector('.fullscreenMode').classList.add('active')
+})})
+
 // Splide Js Render
 document.querySelectorAll('.splide-Home').forEach(function (el) {
   new Splide(el, {
